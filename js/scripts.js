ac60d7f8977efcf7b34f42817ea58c4c29a6b04c
ggugliotta <gabriellagugliotta96@gmail.com>
2023-04-24 01:42:01 -0500

    input for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,34 @@
-let pokemonList = [
+let pokemonList = (function () { [
   
 ];
 
 pokemonList = [ //pokemon objects 
                    {
-                    name:'Squirtle (height: 1.08)', 
+                    name:'Squirtle', 
                     height:'1.08', 
                     type: ['water', 'dragon']
                   }, 
                   {
-                    name:' Charmander (height: 2.00)', 
+                    name:' Charmander', 
                     height:'2.00', 
                     type: ['dragon', 'monster']
                   },
                   {
-                    name: ' Clefairy (height: 2.00)', 
+                    name: ' Clefairy', 
                     height:'2.00', type: ['fairy']
                   },
                   {
-                    name: ' Charizard (height:5.07)',
+                    name: ' Charizard ',
                     height: '5.07',
                     type: ['flame']
                   },
                   {
-                    name: ' Arbok (height: 11.06)',
+                    name: ' Arbok ',
                     height: '11.06', type: ['Cobra']
                   }]
                   
-for (let i = 0; i <= pokemonList.length; i++) { 
-  // this if statement will index = 0 
-  if (pokemonList[i].height <10 && pokemonList[i].height >5){
-    document.write(pokemonList[i].name +  " is medium." );
-  }else if (pokemonList[i].height <5){
-    document.write(pokemonList[i].name +  " is small." );
-  }else {
-    document.write(pokemonList[i].name +  " Wow, that's big!" );
-  }
+function myLoopFunction(pokemon) {
+  document.write(pokemon.name + ' is ' + pokemon.height ,' tall and is a ' + pokemon.type , ' type of pokemon. ' );
 }
+pokemonList.forEach(myLoopFunction);
+})();
