6b6abd3cf4508660012edf7634366c0b32d93fbd
ggugliotta <gabriellagugliotta96@gmail.com>
2023-04-26 17:29:19 -0500

    changed for loop to original

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let pokemonRepository = (function () {
-let pokemonArray = []; // empty array
+let repository = []; // empty array
 
-pokemonArray = [ //pokemon objects 
+repository = [ //pokemon objects 
                    {
                     name:'Squirtle', 
                     height:'1.08', 
@@ -24,40 +23,11 @@ pokemonArray = [ //pokemon objects
                   {
                     name: ' Arbok ',
                     height: '11.06', type: ['Cobra']
-                  }]
-    
-  function add(pokemon) {
-   if (
-     typeof pokemon === "object" &&
-     "name" in pokemon &&
-     "height" in pokemon &&
-     "type" in pokemon
-   ) {
-    pokemonRepository.push(pokemon); 
-   } else {
-    console.log ("pokemon is not correct");
-   }
+                  }]       
 
-  }   
-  function getAll() {
-    return pokemonRepository;
+for (let i = 0; i <= repository.length; i++) { 
+  if (repository[i].height <12){
+    document.write(repository[i].name + ',' + repository[i].height  + ',' + repository[i].type );
   }
-  
-  return {
-  add: add,
-  getAll: getAll,
-  };
-})();
+ }
 
-console.log(pokemonRepository.getAll()); // []
-pokemonRepository.add({ name: 'Squirtle' });
-console.log(pokemonRepository.getAll()); // [ { name: 'Squirtle' } ]
-
-forEach(function (getAll) {
-  let size; 
-  if (item.height > 7) {
-    size = "It's GIGANTIC!";
-   } else {
-      size = "It's a little baby pokemon, how cute!";
-    }
-  });
