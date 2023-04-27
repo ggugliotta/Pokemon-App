bd8c177ce5588ab7399e25d17ea802892a151d36
ggugliotta <gabriellagugliotta96@gmail.com>
2023-04-27 13:15:14 -0500

    array formatting changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let repository = []; // empty array
 let pokemonRepository = (function () {
+    let pokemonList = [] // empty array
 
 repository = [ //pokemon objects 
                    {
@@ -25,6 +26,34 @@ repository = [ //pokemon objects
                     name: ' Arbok ',
                     height: '11.06', type: ['Cobra']
                   }]       
+    pokemonList = [
+        //pokemon objects
+        {
+            name: 'Squirtle',
+            height: '1.08',
+            type: ['water', 'dragon'],
+        },
+        {
+            name: ' Charmander',
+            height: '2.00',
+            type: ['dragon', 'monster'],
+        },
+        {
+            name: ' Clefairy',
+            height: '2.00',
+            type: ['fairy'],
+        },
+        {
+            name: ' Charizard ',
+            height: '5.07',
+            type: ['flame'],
+        },
+        {
+            name: ' Arbok ',
+            height: '11.06',
+            type: ['Cobra'],
+        },
+    ]
 
 for (let i = 0; i <= repository.length; i++) { 
   if (repository[i].height <12){
